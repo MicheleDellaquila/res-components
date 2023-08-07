a638fffe1684c9d56e55b72c5a908f2c8c4c5aac
MicheleDellaquila <micheledellaquila2003@gmail.com>
2023-08-07 19:31:53 +0200

    minor fix

--- components/ui/button/button.js
+++ components/ui/button/button.js
@@ -1,19 +1,23 @@
-import React from "react"
-import "./button.scss"
+import React from "react";
+import "./button.scss";
 
-const Button = ({ isDisabled, size, variant, type, onClick, children }) => {
-  const className = `Btn Btn--${size} Btn--${variant}`
+export const Button = ({
+  isDisabled,
+  size,
+  variant,
+  type,
+  onClick,
+  children,
+}) => {
+  const className = `Btn Btn--${size} Btn--${variant}`;
 
   return (
     <button
       disabled={isDisabled}
       type={type}
       className={className}
-      onClick={onClick}
-    >
+      onClick={onClick}>
       {children}
     </button>
-  )
-}
-
-export default Button
+  );
+};
