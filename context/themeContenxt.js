40ca46d4ec7e5f17483e83cec82b7b5950aef7a2
MicheleDellaquila <micheledellaquila2003@gmail.com>
2023-08-10 15:12:42 +0200

    minor fix themeContext

--- context/themeContenxt.js
+++ context/themeContenxt.js
@@ -1,15 +1,28 @@
-import { createContext, useLayoutEffect, useMemo } from "react";
+import React, { createContext, useLayoutEffect, useMemo } from "react";
 import { setTheme } from "../utility/setTheme";
 
 /* context */
 export const ThemeContext = createContext({});
+
+/* styles example */
+// const styles = { primary: { color: "#fffff" } };
+// const styles = {
+//   primary: {
+//     color: { 50: "#FEFDF3" },
+//     contrastText: "#ffffff",
+//   },
+//   secondary: {
+//     color: "#F7E987",
+//     contrastText: "#000000",
+//   },
+// };
 
 export const ThemeProvider = ({ children, styles }) => {
   const stylesMemorized = useMemo(() => styles, [styles]);
 
   /* set styles in root variable */
   useLayoutEffect(() => {
-    if (stylesMemorized && stylesMemorized.length > 0)
+    if (stylesMemorized && Object.keys(stylesMemorized) > 0)
       setTheme(stylesMemorized);
   }, [setTheme, stylesMemorized]);
 
