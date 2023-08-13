139a1d3cead6d0512289d67e8afa000c6aff2535
MicheleDellaquila <micheledellaquila2003@gmail.com>
2023-08-13 20:39:13 +0200

    new import files

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Button } from "./button";
+export { default as ButtonLink } from "./buttonLink";
+export { default as Spinner } from "./spinner";
--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import Button from "./components/button/button";
-import ButtonLink from "./components/buttonLink/buttonLink";
-import Spinner from "./components/spinner/spinner";
-import ThemeProvider from "./context/themeContext";
-import useChangeTheme from "./hook/useChangeTheme";
-
-/* object with all files to export */
-const index = {
-  ThemeProvider,
-  Button,
-  ButtonLink,
-  Spinner,
-  useChangeTheme,
-};
-
-export default index;
+export * from "./components";
